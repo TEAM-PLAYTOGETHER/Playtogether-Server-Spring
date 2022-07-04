70690e131e481d24d03769f9e74cd4536fdd9f59
LeeJejune <leejejune0625@gmail.com>
2022-07-04 16:46:32 +0900

    initial commit :)

---
+++ src/main/java/com/deploy/playtogether/PlaytogetherApplication.java
@@ -0,0 +1,13 @@
+package com.deploy.playtogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaytogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaytogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deploy/playtogether/PlaytogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.playtogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaytogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
