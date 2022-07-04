d511da022094b0f0ae501cde26f82a79f1f9aa79
LeeJejune <leejejune0625@gmail.com>
2022-07-04 17:34:12 +0900

    test: querydsl 테스트

--- src/test/java/com/deploy/playtogether/PlaytogetherApplicationTests.java
+++ src/test/java/com/deploy/playtogether/PlaytogetherApplicationTests.java
@@ -1,13 +1,38 @@
 package com.deploy.playtogether;
 
+import com.deploy.playtogether.domain.test.Hello;
+import com.deploy.playtogether.domain.test.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import javax.persistence.EntityManager;
 
 @SpringBootTest
+@Transactional
 class PlaytogetherApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
+
 
 }
