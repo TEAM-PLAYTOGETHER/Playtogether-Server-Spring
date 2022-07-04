6c9ddd1c83479354b6b00db9e16679136eaa3699
LeeJejune <leejejune0625@gmail.com>
2022-07-04 17:33:58 +0900

    add: 테스트 controller, entity 추가

---
+++ src/main/java/com/deploy/playtogether/domain/test/Hello.java
@@ -0,0 +1,17 @@
+package com.deploy.playtogether.domain.test;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class Hello {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
