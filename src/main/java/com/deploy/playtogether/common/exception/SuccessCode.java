5fac5db1b0e1123dd4ca1946bc1cb487a41312c3
LeeJejune <leejejune0625@gmail.com>
2022-07-23 15:05:38 +0900

    add: Exception 처리를 위한 ErrorCode Enum 클래스 추가

---
+++ src/main/java/com/deploy/playtogether/common/exception/SuccessCode.java
@@ -0,0 +1,21 @@
+package com.deploy.playtogether.common.exception;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import static com.deploy.playtogether.common.exception.StatusCode.SUCCESS;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum SuccessCode {
+
+    SIGNUP_SUCCESS(SUCCESS, "회원가입이 완료되었습니다.");
+
+    private final StatusCode statusCode;
+    private final String message;
+
+    public int getStatus(){
+        return statusCode.getStatus();
+    }
+}
