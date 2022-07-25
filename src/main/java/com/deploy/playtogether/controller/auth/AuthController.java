e458d56f2cf691190a9b030eeb618c81bb8735dc
LeeJejune <leejejune0625@gmail.com>
2022-07-25 20:00:53 +0900

    feat: 로그인 Controller 개발 / DTO 매핑

--- src/main/java/com/deploy/playtogether/controller/auth/AuthController.java
+++ src/main/java/com/deploy/playtogether/controller/auth/AuthController.java
@@ -2,6 +2,7 @@ package com.deploy.playtogether.controller.auth;
 
 import com.deploy.playtogether.common.dto.ApiResponse;
 import com.deploy.playtogether.common.exception.SuccessCode;
+import com.deploy.playtogether.controller.auth.dto.request.LoginRequestDto;
 import com.deploy.playtogether.controller.auth.dto.request.SignUpRequestDto;
 import com.deploy.playtogether.controller.auth.dto.response.LoginResponse;
 import com.deploy.playtogether.service.auth.AuthService;
@@ -30,5 +31,14 @@ public class AuthController {
         Long userId = authService.signUp(request.toServiceDto());
         httpSession.setAttribute(USER_ID, userId);
         return ApiResponse.success(SuccessCode.SIGNUP_SUCCESS, LoginResponse.of(httpSession.getId(), userId));
+    }
+
+    @ApiOperation("로그인 페이지 - 로그인을 요청합니다")
+    @PostMapping("/v1/login")
+    public ApiResponse<LoginResponse> login(@Valid @RequestBody LoginRequestDto request) {
+        AuthService authService = authServiceProvider.getAuthService(request.getSocialType());
+        Long userId = authService.login(request.toServiceDto());
+        httpSession.setAttribute(USER_ID, userId);
+        return ApiResponse.success(SuccessCode.LOGIN_SUCCESS, LoginResponse.of(httpSession.getId(), userId));
     }
 }
