ec37d48908fbf276ba3d6504e9628c10b08e250e
LeeJejune <leejejune0625@gmail.com>
2022-08-18 18:33:06 +0900

    feat: 번개 이미지 Repository 생성

---
+++ src/main/java/com/deploy/playtogether/domain/light/repository/LightImageRepository.java
@@ -0,0 +1,7 @@
+package com.deploy.playtogether.domain.light.repository;
+
+import com.deploy.playtogether.domain.light.LightImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LightImageRepository extends JpaRepository<LightImage, Long> {
+}
