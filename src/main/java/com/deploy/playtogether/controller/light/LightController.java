071a650d98fcc287e50a82b2e7875313afefb22f
LeeJejune <leejejune0625@gmail.com>
2022-08-18 18:34:08 +0900

    fix: 번개 이미지를 위한 번개 생성 Controller 수정

--- src/main/java/com/deploy/playtogether/controller/light/LightController.java
+++ src/main/java/com/deploy/playtogether/controller/light/LightController.java
@@ -4,25 +4,30 @@ import com.deploy.playtogether.common.dto.ApiResponse;
 import com.deploy.playtogether.common.exception.SuccessCode;
 import com.deploy.playtogether.controller.light.dto.request.LightRequestDto;
 import com.deploy.playtogether.service.light.LightService;
+import com.deploy.playtogether.service.light.S3Service;
 import com.deploy.playtogether.service.light.dto.response.LightResponseDto;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class LightController {
     private final LightService lightService;
+    private final S3Service s3Service;
 
     @ApiOperation("[인증] 번개 생성 API")
-    @PostMapping("/light/add/{crewId}/{userId}")
-    public ApiResponse<LightResponseDto> createLight(@PathVariable Long userId, @PathVariable Long crewId, @RequestBody @Valid LightRequestDto request){
-        return ApiResponse.success(SuccessCode.LIGHT_ADD_SUCCESS, lightService.createLight(userId, crewId, request.toServiceDto()));
+    @PostMapping(value = "/light/add/{crewId}/{userId}", consumes = {"multipart/form-data"})
+    public ApiResponse<LightResponseDto> createLight(@PathVariable Long userId, @PathVariable Long crewId,
+                                                     @ModelAttribute @Valid LightRequestDto request){
+        List<String> imgPaths = s3Service.upload(request.getImages());
+        return ApiResponse.success(SuccessCode.LIGHT_ADD_SUCCESS, lightService.createLight(userId, crewId, request.toServiceDto(), imgPaths));
     }
 }
 
