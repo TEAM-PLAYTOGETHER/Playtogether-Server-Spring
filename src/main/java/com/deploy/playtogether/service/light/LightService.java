9453714914e227b6bce131daa36f201f1fac5abd
LeeJejune <leejejune0625@gmail.com>
2022-08-18 18:34:35 +0900

    fix: 번개 이미지를 위한 번개 생성 Service 수정

--- src/main/java/com/deploy/playtogether/service/light/LightService.java
+++ src/main/java/com/deploy/playtogether/service/light/LightService.java
@@ -4,6 +4,8 @@ import com.deploy.playtogether.common.exception.model.NotFoundException;
 import com.deploy.playtogether.domain.crew.Crew;
 import com.deploy.playtogether.domain.crew.repository.CrewRepository;
 import com.deploy.playtogether.domain.light.Light;
+import com.deploy.playtogether.domain.light.LightImage;
+import com.deploy.playtogether.domain.light.repository.LightImageRepository;
 import com.deploy.playtogether.domain.light.repository.LightRepository;
 import com.deploy.playtogether.domain.user.User;
 import com.deploy.playtogether.domain.user.UserRepository;
@@ -13,22 +15,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class LightService {
     private final LightRepository lightRepository;
+    private final LightImageRepository lightImageRepository;
     private final UserRepository userRepository;
     private final CrewRepository crewRepository;
 
     @Transactional
-    public LightResponseDto createLight(Long userId, Long crewId, LightDto request) {
+    public LightResponseDto createLight(Long userId, Long crewId, LightDto request, List<String> imagePath) {
         User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("존재하지 않는 유저입니다."));
         Crew crew = crewRepository.findById(crewId).orElseThrow(() -> new NotFoundException("존재하지 않는 동아리 입니다."));
         Light light = lightRepository.save(Light.newInstance(
                 request.getTitle(),
                 request.getPlace(),
                 request.getPeopleCount(),
-                request.getLightImages(),
                 request.getDescription(),
                 request.getDate(),
                 request.getTime(),
@@ -36,18 +41,30 @@ public class LightService {
                 user,
                 crew
         ));
+        List<String> lightImageList = saveLightImage(imagePath, light);
+
         return LightResponseDto.of(
                 light.getId(),
                 light.getCategory(),
                 light.getTitle(),
                 light.getDate(),
                 light.getTime(),
-                light.getImageUrls(),
+                lightImageList,
                 light.getPlace(),
                 light.getPeopleCnt(),
                 light.getDescription(),
                 light.getUser().getId(),
                 light.getCrew().getId()
         );
+    }
+
+    private List<String> saveLightImage(List<String> imagePath, Light light) {
+        List<String> imgList = new ArrayList<>();
+        for (String imgUrl : imagePath) {
+            LightImage img = new LightImage(imgUrl, light);
+            lightImageRepository.save(img);
+            imgList.add(img.getImgUrl());
+        }
+        return imgList;
     }
 }
