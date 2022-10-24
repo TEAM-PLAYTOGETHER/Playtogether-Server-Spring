b6fd6630e7b6e88c751767d9fb5d1ffeb40ee45e
LeeJejune <leejejune0625@gmail.com>
2022-10-24 14:18:57 +0900

    fix: session 제거

--- src/main/java/com/deploy/playtogether/config/session/SessionConstants.java
+++
@@ -1,9 +0,0 @@
-package com.deploy.playtogether.config.session;
-
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class SessionConstants {
-    public static final String USER_ID = "USER_ID";
-}
