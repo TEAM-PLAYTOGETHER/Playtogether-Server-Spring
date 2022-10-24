e3f5676f6f8d075b0e0c37deb6272dd97d477b19
LeeJejune <leejejune0625@gmail.com>
2022-10-24 14:20:15 +0900

    refactor: Light Service 리펙터링

--- src/main/java/com/deploy/playtogether/service/light/LightService.java
+++ src/main/java/com/deploy/playtogether/service/light/LightService.java
@@ -37,6 +37,7 @@ import static com.deploy.playtogether.common.exception.ErrorCode.NOT_FOUND_USER_
 @RequiredArgsConstructor
 @Service
 public class LightService {
+
     private final LightRepository lightRepository;
     private final LightImageRepository lightImageRepository;
     private final LightUserRepository lightUserRepository;
@@ -45,59 +46,69 @@ public class LightService {
     private final ReportLightRepository reportLightRepository;
 
     @Transactional
-    public void createLight(final Long userId, final Long crewId, final LightDto request, final List<String> imagePath) {
-        final User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("존재하지 않는 유저입니다."
+    public void createLight(final Long userId, final Long crewId, final LightDto request,
+        final List<String> imagePath) {
+        final User user = userRepository.findById(userId)
+            .orElseThrow(() -> new NotFoundException("존재하지 않는 유저입니다."
                 , NOT_FOUND_USER_EXCEPTION));
-        final Crew crew = crewRepository.findById(crewId).orElseThrow(() -> new NotFoundException("존재하지 않는 동아리 입니다."
+        final Crew crew = crewRepository.findById(crewId)
+            .orElseThrow(() -> new NotFoundException("존재하지 않는 동아리 입니다."
                 , NOT_FOUND_EXCEPTION));
         final Light light = lightRepository.save(Light.newInstance(
-                request.getTitle(),
-                request.getPlace(),
-                request.getPeopleCount(),
-                request.getDescription(),
-                request.getDate(),
-                request.getTime(),
-                request.getCategory(),
-                user,
-                crew
+            request.getTitle(),
+            request.getPlace(),
+            request.getPeopleCount(),
+            request.getDescription(),
+            request.getDate(),
+            request.getTime(),
+            request.getCategory(),
+            user,
+            crew
         ));
         saveLightImage(imagePath, light);
         lightUserRepository.save(LightUser.newInstance(light.getUser(), light));
     }
 
     @Transactional
-    public void reportLight(final Long crewId, final Long lightId, final Long userId, final ReportLightDto request) {
-        crewRepository.findById(crewId).orElseThrow(() -> new NotFoundException("존재하지 않는 동아리 입니다.", NOT_FOUND_EXCEPTION));
-        final User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("존재하지 않는 유저입니다.", NOT_FOUND_USER_EXCEPTION));
-        final Light light = lightRepository.findById(lightId).orElseThrow(() -> new NotFoundException("존재하지 않는 번개입니다.", NOT_FOUND_EXCEPTION));
+    public void reportLight(final Long crewId, final Long lightId, final Long userId,
+        final ReportLightDto request) {
+        crewRepository.findById(crewId)
+            .orElseThrow(() -> new NotFoundException("존재하지 않는 동아리 입니다.", NOT_FOUND_EXCEPTION));
+        final User user = userRepository.findById(userId)
+            .orElseThrow(() -> new NotFoundException("존재하지 않는 유저입니다.", NOT_FOUND_USER_EXCEPTION));
+        final Light light = lightRepository.findById(lightId)
+            .orElseThrow(() -> new NotFoundException("존재하지 않는 번개입니다.", NOT_FOUND_EXCEPTION));
 
         checkLightReport(user, light);
 
         reportLightRepository.save(
-                ReportLight.newInstance(
-                        user,
-                        request.getReportReason(),
-                        light
-                )
+            ReportLight.newInstance(
+                user,
+                request.getReportReason(),
+                light
+            )
         );
     }
 
     @Transactional
     public List<LightResponse> getHotLight(final Long crewId) {
         crewRepository.findById(crewId).orElseThrow(() -> new NotFoundException("존재하지 않는 동아리 입니다."
-                , NOT_FOUND_EXCEPTION));
+            , NOT_FOUND_EXCEPTION));
         final Pageable page = PageRequest.of(0, 5);
         final List<Light> hotLights = lightRepository.findAllByOrderByScpCntDesc(page);
 
         return getLightResponseList(hotLights);
     }
+
     @Transactional
-    public LightResponseUsingCursorResponse getOpenLight(final Long crewId, final Long userId, final int size, final Long lastLightId) {
+    public LightResponseUsingCursorResponse getOpenLight(final Long crewId, final Long userId,
+        final int size, final Long lastLightId) {
         userRepository.findById(userId).orElseThrow(() -> new NotFoundException("존재하지 않는 유저입니다."
-                , NOT_FOUND_USER_EXCEPTION));
+            , NOT_FOUND_USER_EXCEPTION));
         crewRepository.findById(crewId).orElseThrow(() -> new NotFoundException("존재하지 않는 동아리 입니다."
-                , NOT_FOUND_EXCEPTION));
-        final List<Light> openLights = lightRepository.findAllByUserIdUsingCursor(userId, size, lastLightId);
+            , NOT_FOUND_EXCEPTION));
+        final List<Light> openLights = lightRepository.findAllByUserIdUsingCursor(userId, size,
+            lastLightId);
         final List<LightResponse> lightResponseList = getLightResponseList(openLights);
         return getLightResponseUsingCursorResponse(size, lightResponseList);
     }
@@ -105,7 +116,7 @@ public class LightService {
     @Transactional
     public List<LightResponse> getNewLight(final Long crewId) {
         crewRepository.findById(crewId).orElseThrow(() -> new NotFoundException("존재하지 않는 동아리 입니다."
-                , NOT_FOUND_EXCEPTION));
+            , NOT_FOUND_EXCEPTION));
         final Pageable page = PageRequest.of(0, 5);
         final List<Light> newLights = lightRepository.findAllByOrderByCreatedAtDesc(page);
 
@@ -116,36 +127,40 @@ public class LightService {
     public List<String> saveLightImage(final List<String> imagePath, final Light light) {
         final List<String> imgList = new ArrayList<>();
         imagePath.stream()
-                .map(imgUrl -> LightImage.newInstance(imgUrl, light))
-                .forEach(img -> {
-                    lightImageRepository.save(img);
-                    imgList.add(img.getImgUrl());
-                });
+            .map(imgUrl -> LightImage.newInstance(imgUrl, light))
+            .forEach(img -> {
+                lightImageRepository.save(img);
+                imgList.add(img.getImgUrl());
+            });
         return imgList;
     }
 
     private void checkLightReport(final User user, final Light light) {
-        final Optional<ReportLight> reportLight = reportLightRepository.findByUserIdAndLightId(user.getId(), light.getId());
+        final Optional<ReportLight> reportLight = reportLightRepository.findByUserIdAndLightId(
+            user.getId(), light.getId());
         if (reportLight.isPresent()) {
             throw new ConflictException("해당 번개를 이미 신고한 상태입니다.", ErrorCode.CONFLICT_EXCEPTION);
         }
     }
 
     @NotNull
-    private LightResponseUsingCursorResponse getLightResponseUsingCursorResponse(int size, List<LightResponse> lightResponseList) {
-        if (size > lightResponseList.size()){
+    private LightResponseUsingCursorResponse getLightResponseUsingCursorResponse(final int size,
+        final List<LightResponse> lightResponseList) {
+        if (size > lightResponseList.size()) {
             return LightResponseUsingCursorResponse.of(lightResponseList, -1L);
         }
-        return LightResponseUsingCursorResponse.of(lightResponseList, lightResponseList.get(size - 1).getLightId());
+        return LightResponseUsingCursorResponse.of(lightResponseList,
+            lightResponseList.get(size - 1).getLightId());
     }
 
     @NotNull
-    private List<LightResponse> getLightResponseList(List<Light> lights) {
+    private List<LightResponse> getLightResponseList(final List<Light> lights) {
         return lights.stream()
-                .map(l -> LightResponse.of(l.getId(), l.getCategory(), l.getTitle(), l.getDate(), l.getTime(),
-                        l.getPlace(), l.getPeopleCnt(), l.getScpCnt(),
-                        l.getLightMemberCnt()
-                )).collect(Collectors.toList());
+            .map(l -> LightResponse.of(l.getId(), l.getCategory(), l.getTitle(), l.getDate(),
+                l.getTime(),
+                l.getPlace(), l.getPeopleCnt(), l.getScpCnt(),
+                l.getLightMemberCnt()
+            )).collect(Collectors.toList());
     }
 
 }
