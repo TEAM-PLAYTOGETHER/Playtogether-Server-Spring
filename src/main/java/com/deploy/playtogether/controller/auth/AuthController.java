c82fc3d0fcfccf21ad5da2c1197506ba91390d04
LeeJejune <leejejune0625@gmail.com>
2022-10-24 14:20:41 +0900

    feat: AuthController session 제거

--- src/main/java/com/deploy/playtogether/controller/auth/AuthController.java
+++ src/main/java/com/deploy/playtogether/controller/auth/AuthController.java
@@ -2,11 +2,16 @@ package com.deploy.playtogether.controller.auth;
 
 import com.deploy.playtogether.common.dto.ApiResponse;
 import com.deploy.playtogether.common.exception.SuccessCode;
+import com.deploy.playtogether.config.jwt.JwtService;
 import com.deploy.playtogether.controller.auth.dto.request.LoginRequestDto;
 import com.deploy.playtogether.controller.auth.dto.request.SignUpRequestDto;
 import com.deploy.playtogether.controller.auth.dto.response.LoginResponse;
+import com.deploy.playtogether.domain.user.User;
+import com.deploy.playtogether.domain.user.UserRepository;
 import com.deploy.playtogether.service.auth.AuthService;
 import com.deploy.playtogether.service.auth.AuthServiceProvider;
+import com.deploy.playtogether.service.user.UserService;
+import com.deploy.playtogether.service.user.UserServiceUtils;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,21 +21,22 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
-import static com.deploy.playtogether.config.session.SessionConstants.USER_ID;
 
 @RequiredArgsConstructor
 @RestController
 public class AuthController {
-    private final HttpSession httpSession;
+    private final UserService userService;
+    private final UserRepository userRepository;
     private final AuthServiceProvider authServiceProvider;
+    private final JwtService jwtService;
 
     @ApiOperation("회원가입 페이지 - 회원가입을 요청합니다")
     @PostMapping("/v1/signup")
     public ApiResponse<LoginResponse> signUp(@Valid @RequestBody final SignUpRequestDto request) {
         final AuthService authService = authServiceProvider.getAuthService(request.getSocialType());
         final Long userId = authService.signUp(request.toServiceDto());
-        httpSession.setAttribute(USER_ID, userId);
-        return ApiResponse.success(SuccessCode.SIGNUP_SUCCESS, LoginResponse.of(httpSession.getId(), userId));
+        final String token = jwtService.issuedToken(String.valueOf(userId), "USER", 60 * 60 * 24 * 30L);
+        return ApiResponse.success(SuccessCode.SIGNUP_SUCCESS, LoginResponse.of(token, userId));
     }
 
     @ApiOperation("로그인 페이지 - 로그인을 요청합니다")
@@ -38,7 +44,9 @@ public class AuthController {
     public ApiResponse<LoginResponse> login(@Valid @RequestBody final LoginRequestDto request) {
         final AuthService authService = authServiceProvider.getAuthService(request.getSocialType());
         final Long userId = authService.login(request.toServiceDto());
-        httpSession.setAttribute(USER_ID, userId);
-        return ApiResponse.success(SuccessCode.LOGIN_SUCCESS, LoginResponse.of(httpSession.getId(), userId));
+        final String token = jwtService.issuedToken(String.valueOf(userId), "USER", 60 * 60 * 24 * 30L);
+
+        User findUser = UserServiceUtils.findUserById(userRepository, userId);
+        return ApiResponse.success(SuccessCode.LOGIN_SUCCESS, LoginResponse.of(token, userId));
     }
 }
